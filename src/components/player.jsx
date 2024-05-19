67e9215becf28d6cf2232ef4d618a0c3fb804775
mace <masonbunyan@hotmail.com>
2024-05-19 21:36:27 +0100

    Add gameboard design without functionality.

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import { useState } from "react";
 
-const Player = ({ name, symbol }) => {
+// USED TERNARY EXPRESSION TO CONDITIONALLY RENDER THE HTML.
+
+const Player = ({ initialName, symbol }) => {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function editClicked() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+
+    setPlayerName(event.target.value);
+
   }
   return (
     <li>
       <span className="player">
         {isEditing ? (
-          <input type="text" required />
+          <input type="text" required value={playerName} onChange={handleChange}/>
         ) : (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         )}
 
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={editClicked}>Edit</button>
+      <button onClick={editClicked}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 };
