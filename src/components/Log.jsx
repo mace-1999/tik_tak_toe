becb954b7e3a4cf0b41fc1657f1ea7a93d52ce43
mace <masonbunyan@hotmail.com>
2024-05-24 19:40:27 +0100

    Add log message.

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export const Log = () => {
+export const Log = ({ turns }) => {
   return (
-   <ol id='log'>
-
-   </ol>
-  )
-}
+    <ol id="log">
+      {turns.map((turn, ind) => (
+        <li key={ind}>
+          <h3>{turn.player} selected {turn.square.row} {turn.square.col}</h3>
+        </li>
+      ))}
+    </ol>
+  );
+};
